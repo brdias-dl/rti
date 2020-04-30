9c09ca6965fecc60c1019ab6e47376f5542289e1
brdias-dl <2190974@my.ipleiria.pt>
2020-04-30 17:59:39 +0100

    Commit

--- js/index.js
+++ js/index.js
@@ -1,15 +1,28 @@
-function createAppliance(){
-    var appliancesBody = document.getElementById("appliancesBody");
+var appliancesBody = document.getElementById("appliancesBody");
+
+
+
+$("#createApplianceBtn").on("click", (name, icon) => {
+    var name = document.getElementById("name").value;
+    var icon = document.getElementById("icon").value;
+
+    switch (icon) {
+        case "tv":
+            icon = "tv";
+            break;
+
+        default:
+            break;
+    }
 
     appliancesBody.innerHTML += `
     <div class="appliances-item">
         <div class="appliances-item-image">
-            <i class="fas fa-2x fa-${document.getElementById("icon").value}"></i>
+            <i class="fas fa-2x fa-${icon}"></i>
         </div>
         <div class="appliances-item-title">
-            <h6>${document.getElementById("name").value}</h6>
+            <h6>${name}</h6>
         </div>
     </div>
-    `
-    ;
-}
+    `;
+});
