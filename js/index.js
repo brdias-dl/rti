47300d3862124a8239194c43d6104ca5c77a9c3b
Bruno Dias <57989458+brdias-dl@users.noreply.github.com>
2020-05-14 17:30:26 +0100

    asd

--- js/index.js
+++ js/index.js
@@ -1,4 +1,83 @@
 var iconclicked = '';
+var iconsArray = {
+    "Bell": "fa-bell",
+    "Play": "fa-play",
+    "Random": "fa-random",
+    "Undo": "fa-undo",
+    "Recycle": "fa-recycle",
+    "Backward": "fa-backward",
+    "Eject": "fa-eject",
+    "Expand": "fa-expand",
+    "Fast Backward": "fa-fast-backward",
+    "Fast Forward": "fa-fast-forward",
+    "Forward": "fa-forward",
+    "Headphone": "fa-headphones",
+    "Music": "fa-music",
+    "Pause": "fa-pause",
+    "Stop": "fa-stop",
+    "Television": "fa-tv",
+    "Video": "fa-video",
+    "Volume Down": "fa-volume-down",
+    "Volume Off": "fa-volume-off",
+    "Volume Up": "fa-volume-up",
+    "Car": "fa-car",
+    "Charging Station": "fa-charging-station",
+    "Apple": "fa-apple-alt",
+    "Cloud Sun": "fa-cloud-sun",
+    "Mountain": "fa-mountain",
+    "Tree": "fa-tree",
+    "Wind": "fa-wind",
+    "Blender": "fa-blender",
+    "Coffee": "fa-coffee",
+    "Envelope": "fa-envelope",
+    "Glasses": "fa-glasses",
+    "Sticky Note": "fa-sticky-note",
+    "Phone": "fa-phone-alt",
+    "Baby": "fa-baby",
+    "Baby Carriage": "fa-baby",
+    "Bath": "fa-bath",
+    "Birthday Cake": "fa-birthday-cake",
+    "Gamepad": "fa-gamepad",
+    "Mitten": "fa-mitten",
+    "T-shirt": "fa-tshirt",
+    "Bug": "fa-bug",
+    "Fire Extinguisher": "fa-fire-extinguisher",
+    "Desktop": "fa-desktop",
+    "Laptop": "fa-laptop",
+    "Mobile": "fa-mobile-alt",
+    "Plug": "fa-plug",
+    "Power": "fa-power-off",
+    "Tablet": "fa-tablet-alt",
+    "Toolbox": "fa-toolbox",
+    "Dollar": "fa-dollar-sign",
+    "Euro": "fa-euro-sign",
+    "Hryvnia": "fa-hryvnia",
+    "Libra": "fa-libra-sign",
+    "Money Bill": "fa-money-bill",
+    "Pound": "fa-pound-sign",
+    "Ruble": "fa-ruble-sign",
+    "Rupee": "fa-rupee-sign",
+    "Shekel": "fa-shekel-sign",
+    "Tenge": "fa-tenge",
+    "Won": "fa-won-sign",
+    "Yen": "fa-yen-sign",
+    "Calendar": "fa-calendar-alt",
+    "Clock": "fa-clock",
+    "Eye": "fa-eye",
+    "Palette": "fa-palette",
+    "Music": "fa-music",
+    "Fan": "fa-fan",
+    "Lightbulb": "fa-lightbulb",
+    "Solar Panel": "fa-solar-panel",
+    "Sun": "fa-sun",
+    "Book": "fa-book",
+    "Wheelchair": "fa-wheelchair",
+    "Wifi": "fa-wifi",
+    "Shower": "fa-shower",
+    "Bed": "fa-bed",
+    "Door Closed": "fa-door-closed",
+    "Door Open": "fa-door-open"
+}
 
 document.getElementById("createApplianceBtn").onclick = function(name) {
     var appliancesBody = document.getElementById("appliancesBody");
@@ -37,12 +116,6 @@ document.getElementById("createPreviewBtn").onclick = function(name) {
     `;
 };
 
-$(".appliance-icon").click(function(event) {
-    if (event.target.children[0].classList[1].match("^fa-")) {
-        iconclicked = event.target.children[0].classList[1];
-    }
-});
-
 $("#loginpress").click(() => {
     if(!$("#loginusername").value == 0 || !$("#loginpassword").value == 0){
         alert("Tem que introduzir algo nos campos");
@@ -55,3 +128,21 @@ $("#loginpress").click(() => {
     setTimeout(() => $("#homepage").slideToggle(500), 500);
 });
 
+$(document).ready(() => {
+    var iconsappliances = document.getElementById("iconsappliances");
+    for (var name in iconsArray) {
+        iconsappliances.innerHTML += `
+        <button class="dropdown-item appliance-icon" type="button">
+            <i class="fas ${iconsArray[name]}"></i>
+            &nbsp;&nbsp;${name}
+        </button>
+        `;    
+    }
+
+    
+$(".appliance-icon").click(function(event) {
+    if (event.target.children[0].classList[1].match("^fa-")) {
+        iconclicked = event.target.children[0].classList[1];
+    }
+});
+});
